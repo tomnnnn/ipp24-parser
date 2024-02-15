a8f55979018587eeb11e2783faa408bb4339e036
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2024-02-15 11:52:26 +0100

    implement: token type detection using regex

--- modules/lexer.py
+++ modules/lexer.py
@@ -1,11 +1,17 @@
 import sys
-from dataclasses import dataclass, make_dataclass
-from enum import Enum
+import re
+from dataclasses import dataclass
+from enum import Enum, auto
+
 
 class TokenTypes(Enum):
-    INSTR = 1
-    VAR = 2
-    STR = 3
+    HEAD = auto()
+    ALPHANUM = auto()
+    VAR = auto()
+    STR = auto()
+    INT = auto()
+    BOOL = auto()
+    NONE = auto()
     
 @dataclass
 class Token:
@@ -18,7 +24,17 @@ class TokenIterator:
 
     def __init__(self, input_file) -> None:
         self.inputFile = input_file
-        self.current_token = Token(True,TokenTypes.INSTR, "")
+        self.current_token = Token(True,TokenTypes.ALPHANUM, '')
+        
+        # regex for token types definitions
+        self.typeReg = {
+            TokenTypes.HEAD : r'.IPPcode24',
+            TokenTypes.ALPHANUM : r'^[a-zA-Z][\w$%*!?-]*$',
+            TokenTypes.VAR : r'^(?i:(LF|GF))@[a-zA-z][\w$%*!?-]*$',
+            TokenTypes.STR : r'^string@[^\s#]*$',
+            TokenTypes.INT : r'^int@-?(0x|0o)?\d+(_\d+)*$',
+            TokenTypes.BOOL : r'bool@(false|true)'
+       }
 
     def __iter__(self):
         while True:
@@ -33,17 +49,26 @@ class TokenIterator:
                 self.current_token.type = self.__detect_token_type(word)
                 self.current_token.string = word
                 
+                newline = False
                 yield self.current_token
 
     def __detect_token_type(self, string: str) -> TokenTypes:
-        return TokenTypes.INSTR
+        for type in self.typeReg:
+            if re.match(self.typeReg[type],string):
+                if(type == TokenTypes.INT):
+                    # additional integer format check
+                    try:
+                        int(string.split('@')[1],0)
+                    except ValueError:
+                        continue
+                return type
+        return TokenTypes.NONE
     
 
 input_file = sys.stdin
 token_iter = TokenIterator(input_file)
 counter = 0
 for token in token_iter:
-    print(str(counter) + " " + token.string)
+    print(str(counter), end=' ')
+    print(token)
     counter += 1
-
-print(token_iter.current_token)
