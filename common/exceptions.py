81a8cbdbaf19fa56fca2fe7752a7bb40a70712d2
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2024-02-19 15:28:49 +0100

    add exceptions common module

--- common/exceptions.py
+++ common/exceptions.py
@@ -1,2 +1,11 @@
 class LexError(Exception):
     """Signals a lexical error in the input"""
+
+class SyntaxError(Exception):
+    pass
+
+class OpcodeError(Exception):
+    pass
+
+class MissingHeadError(Exception):
+    pass
